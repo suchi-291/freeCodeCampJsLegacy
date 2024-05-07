9d5d1f79f58729a77a8aaf868f76e3a405a2d856
Suchitha Ramachandrapurapu <66826980+suchi-291@users.noreply.github.com>
2024-05-07 21:49:08 +0530

    undefinedNaN.js

---
+++ undefinedNaN.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+//mathematical operartion with undefined gives NaN
+//Concatenating String with undefined, gives a string of undefined
+//When not initialized the value that a variable has is <undefined>
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
