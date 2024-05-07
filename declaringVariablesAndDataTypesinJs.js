fe10893d13971eaf589d31b1fa4f78c59756d810
Suchitha Ramachandrapurapu <66826980+suchi-291@users.noreply.github.com>
2024-05-07 20:33:26 +0530

    Create declaringVariablesAndDataTypesinJs.js

---
+++ declaringVariablesAndDataTypesinJs.js
@@ -0,0 +1,7 @@
+/*8 datatypes - number, string, boolean, null, undefined, symbol, Bigint, Object */
+
+/* Variable names can be made of numbers, letters, and $ or _,
+Should not contain spaces or start with a number*/
+
+var myName;
+
