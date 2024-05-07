1bc972d103264d86ae85469cb666af8acd1da628
Suchitha Ramachandrapurapu <66826980+suchi-291@users.noreply.github.com>
2024-05-07 22:07:17 +0530

    LetvsVar.js

---
+++ LetvsVar.js
@@ -0,0 +1,4 @@
+/*let is a keyword introduced in ES6/ ES2015, When using
+var, there can be accidental redeclaration and unwanted hoisting might occur, with let we can avoid such situations, but it is recommended to use const when we know the value need not be changed and use let when we know it needs to be changed, in this way we can avoid a lot of bugs*/
+let catName = "Oliver";
+let catSound = "Meow!";
