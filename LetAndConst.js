a9c5d72c7249e131f526993b16bdccce2f605227
Suchitha Ramachandrapurapu <66826980+suchi-291@users.noreply.github.com>
2024-05-10 11:43:44 +0530

    Commit 🚀

--- LetAndConst.js
+++ LetAndConst.js
@@ -1,4 +1,4 @@
 const fCC = "freeCodeCamp"; // Change this line
 let fact = "is cool!"; // Change this line
 fact = "is awesome!";
-console.log(fCC, fact); // Change this line
+console.log(fCC, fact); // Change this line 
